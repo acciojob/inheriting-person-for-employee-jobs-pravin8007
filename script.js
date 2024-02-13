7abe59616bf3d33a66e25c0f457b8d978544c489
Pravin Patil <106026547+pravin8007@users.noreply.github.com>
2024-02-13 11:53:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-// complete this js code
-function Person(name, age) {
-	this._name = name;
-	this._age = age ;
-	greet(){
-		console.log(`Hello, my name is ${this._name}, I am ${this._age} years old`);
-	}
+// Create a Person Class
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    greet() {
+        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+    }
 }
 
+// Create an Employee Class (inherits from Person)
 class Employee extends Person {
     constructor(name, age, jobTitle) {
         super(name, age); // Call the constructor of the parent class (Person)
@@ -17,7 +21,6 @@ class Employee extends Person {
         console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
     }
 }
-
 
 // Do not change code below this line
 window.Person = Person;
