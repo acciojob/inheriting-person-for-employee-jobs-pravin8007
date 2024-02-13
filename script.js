ede7b8607f5f44ed4b976cd33e4047081b3c94d2
Pravin Patil <106026547+pravin8007@users.noreply.github.com>
2024-02-13 11:45:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	this._name = name;
+	this._age = age ;
+	greet(){
+		console.log(`Hello, my name is ${this._name}, I am ${this._age} years old`);
+	}
+}
 
-function Employee(name, age, jobTitle) {}
+function Employee(name, age, jobTitle) {
+	super(name,age);
+	this._jobTitle = jobTitle;
+	jobGreet(){
+		console.log(`Hello, my name is ${this._name}, I am ${this._age} years old, and my job title is ${this._jobTitle}`);
+	}
+}
 
 // Do not change code below this line
 window.Person = Person;
